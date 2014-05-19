5ed7017429cd3bb35457041c46bc7ba360fd4d33
phoenix19900219 <zhouchangshi@yeah.net>
2014-05-19 17:32:52 +0800

    aa

--- com.stone.zone.askask/src/com/stone/zone/askask/GuideActivity.java
+++ com.stone.zone.askask/src/com/stone/zone/askask/GuideActivity.java
@@ -3,7 +3,6 @@ package com.stone.zone.askask;
 import org.androidannotations.annotations.EActivity;
 
 import android.app.Activity;
-import android.support.v4.view.ViewPager;
 
 @EActivity(R.layout.activity_guide)
 public class GuideActivity extends Activity {
